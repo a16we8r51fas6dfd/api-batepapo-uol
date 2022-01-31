ecbd7f1569ec3d48d2eb6d9249867bd1c7d3e4ea
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-01-31 13:18:46 -0300

    feat: add process.env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import dayjs from 'dayjs'
 dotenv.config()
 dayjs().format()
 
-const mongoClient = new MongoClient("mongodb://localhost:27017")
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 let db
 mongoClient.connect(() => {
     db = mongoClient.db("o_banco_de_dados_apenas")
