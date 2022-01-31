462a4c38b4286181b975659c694e20b7c044ed38
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-01-31 05:54:38 -0300

    feat: add post messages route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { json } from 'express'
+import express, { json, text } from 'express'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -20,6 +20,12 @@ server.use(cors())
 
 const participantSchema =  joi.object({
     name: joi.string().required()
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
 })
 
 /* participants routes */
@@ -84,6 +90,30 @@ server.delete('/participants', async (req, res) => {
 })
 
 /* messages routes */
+
+server.post('/messages', async (req, res) => {
+    const user = req.headers.user
+    const message = req.body
+
+    const validation = messageSchema.validate(req.body)
+
+    if (validation.error) {
+        res.status(422).send(validation.error.message)
+        return
+    }
+
+    try {
+        await db.collection('messages').insertOne({
+            from: user,
+            ... message,
+            time: dayjs().format('HH:mm:ss')
+        })
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
+
 
 
 
